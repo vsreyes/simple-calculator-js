eb8ced54e29160e3943f5fb824d12808bf891339
vsreyes <virgilio.s.reyes@gmail.com>
2020-11-08 16:18:32 +0100

    Script

---
+++ script.js
@@ -0,0 +1,6 @@
+function getHistory() {
+  return document.getElementById('history-value').innerText;
+}
+function printHistory(num) {
+  document.getElementById('history-value').innerText = num;
+}
